cf5d598b3ed146f573b99cdc5cadb422f5dd67d8
ulugbek <ulugbekaxtamov.001@gmail.com>
2024-01-22 12:17:01 +0500

    add "Waiting 15 seconds for PSQL installation" in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Apply database migrations
-echo "Apply database migrations"
-alembic upgrade head
+# Waiting PSQL
+svc=$1
+echo "Waiting 15 seconds for PSQL installation"
+sleep 15
 
-# Start server
-echo "Starting server"
-uvicorn main:app --host 0.0.0.0 --port 8000
+if [ "$svc" = "web" ]; then
+  # Apply database migrations
+  echo "Apply database migrations"
+  alembic upgrade head
+
+  # Start server
+  echo "Starting server"
+  uvicorn main:app --host 0.0.0.0 --port 8000
+fi
